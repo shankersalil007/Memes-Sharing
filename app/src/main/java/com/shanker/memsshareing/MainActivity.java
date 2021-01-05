b406ecd9862c4af411829ed0e578fe88d0c51ebd
Shanker Salil <shankersalil8@gmail.com>
2021-01-05 15:18:36 +0530

    added progressbar during image loading

--- app/src/main/java/com/shanker/memsshareing/MainActivity.java
+++ app/src/main/java/com/shanker/memsshareing/MainActivity.java
@@ -1,5 +1,6 @@
 package com.shanker.memsshareing;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
@@ -9,6 +10,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -19,6 +21,10 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -34,17 +40,22 @@ public class MainActivity extends AppCompatActivity {
 
     ImageView imageView;
     RequestQueue queue;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageView =(ImageView)findViewById(R.id.imageView);
+        progressBar=(ProgressBar)findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
+        progressBar.bringToFront();
         queue = Volley.newRequestQueue(this);
         loadImage();
     }
 
     void loadImage(){
+        progressBar.setVisibility(View.VISIBLE);
         String url ="https://meme-api.herokuapp.com/gimme";
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
                 new Response.Listener<JSONObject>() {
@@ -53,8 +64,18 @@ public class MainActivity extends AppCompatActivity {
                         try {
                             String imageUrl = null;
                             imageUrl = response.getString("url");
-                            Log.i("msg","image url "+imageUrl);
-                            Glide.with(MainActivity.this).load(imageUrl).into(imageView);
+                            Glide.with(MainActivity.this).load(imageUrl).listener(new RequestListener<Drawable>() {
+                                @Override
+                                public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                                    return false;
+                                }
+
+                                @Override
+                                public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                                    progressBar.setVisibility(View.INVISIBLE);
+                                    return false;
+                                }
+                            }).into(imageView);
                         } catch (JSONException e) {
                             Log.i("msg",e.getMessage());
                             e.printStackTrace();
@@ -67,7 +88,6 @@ public class MainActivity extends AppCompatActivity {
                        Log.i("msg",error.getMessage());
                     }
                 });
-
         queue.add(jsonObjectRequest);
     }
 
